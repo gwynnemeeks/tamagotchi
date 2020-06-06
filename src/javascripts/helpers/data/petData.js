10b0cf9b59a3ce4ac96371da65be3efc35c1c6cb
Gwynne Meeks <meeks.gwynne@gmail.com>
2020-06-06 13:37:05 -0500

    went a little farther than intended

---
+++ src/javascripts/helpers/data/petData.js
@@ -0,0 +1,14 @@
+const pet = [
+  {
+    name: 'Banshee',
+    full: 100,
+    fun: 50,
+    strength: 100,
+    energy: 50,
+    id: 'pet0',
+  },
+];
+
+const getPets = () => pet;
+
+export default { getPets };
